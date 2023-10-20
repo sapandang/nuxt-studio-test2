c23335ebd5f41e344453fb4a2f686b358c0b0e36
SKD <skdang1993@gmail.com>
2023-10-20 23:27:28 +0530

    j

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,21 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  color: {
+    b: {},
+    bl: {}
+  },
+
+  prose: {
+    code: {
+      block: {
+        border: {
+          color: {
+            initial: 'var(--color-blue-200',
+            dark: '#201E1B'
+          }
+        }
+      }
+    }
+  }
 })
